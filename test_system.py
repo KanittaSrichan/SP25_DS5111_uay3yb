831bb018b4fa27d62b0cb66f423066ddfd26b5cf
KanittaSrichan <uay3yb@virginia.edu>
2025-03-06 02:09:58 +0000

    Deleted unnecessary files for LAB-06

--- test_system.py
+++
@@ -1,7 +0,0 @@
-# test_system.py
-
-import platform
-
-def test_os_is_linux():
-    # This test will fail if the OS is not Linux
-    assert platform.system() == "Linux", f"Expected Linux, but got {platform.system()}"
