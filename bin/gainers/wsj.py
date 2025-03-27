b25e0c3e7f27739c136665d9afa55f1bbb08ed47
KanittaSrichan <uay3yb@virginia.edu>
2025-03-27 02:52:50 +0000

    Fixed line too long

--- bin/gainers/wsj.py
+++ bin/gainers/wsj.py
@@ -6,7 +6,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from .base import GainerDownload, GainerProcess  # Local import
+from .base import (
+    GainerDownload,
+    GainerProcess
+)  # Local import
+
 
 # Function to initialize ChromeDriver with proper options
 def get_chrome_driver():
