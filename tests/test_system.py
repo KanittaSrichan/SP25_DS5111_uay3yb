d9e444af91ea5604072df81545aea58348f9e2a3
KanittaSrichan <uay3yb@virginia.edu>
2025-04-09 21:46:05 -0400

    Modified python version test

--- tests/test_system.py
+++ tests/test_system.py
@@ -9,9 +9,16 @@ def test_os_is_linux():
     # This test will fail if the OS is not Linux
     assert platform.system() == "Linux", f"Expected Linux, but got {platform.system()}"
 
+import sys
+
 def test_python_version():
-    # This test will check that the Python version is 3.12
-    expected_version = "3.12"
+    # This test will check that the Python version is either 3.12 or 3.13
+    expected_major_version = 3
+    expected_minor_versions = [12, 13]  # We accept both 3.12 and 3.13
+
     actual_version = sys.version_info
-    assert actual_version.major == 3 and actual_version.minor == 12, \
-        f"Expected Python 3.12, but got Python {actual_version.major}.{actual_version.minor}"
+    assert actual_version.major == expected_major_version, \
+        f"Expected Python 3.x, but got Python {actual_version.major}.{actual_version.minor}"
+
+    assert actual_version.minor in expected_minor_versions, \
+        f"Expected Python 3.12 or 3.13, but got Python {actual_version.major}.{actual_version.minor}"
